5fc4a97e15005a63a16977f34ab445456ad64ed3
Grant Mitchell <soundbreakr1@gmail.com>
2023-01-02 13:59:43 -0700

    logging payload object

--- action.js
+++ action.js
@@ -5,17 +5,18 @@ const github = require('@actions/github');
 const main = async () => {
 try {
   const payload = github.context.payload;
-  const event = core.getInput('event');
-  const owner = event.repository.owner.name
-  const repo = event.repository.name
-  const token = core.getInput('token', { required: true });
-  const octokit = new github.getOctokit(token);
+  console.log(payload);
+  // const event = core.getInput('event');
+  // const owner = event.repository.owner.name
+  // const repo = event.repository.name
+  // const token = core.getInput('token', { required: true });
+  // const octokit = new github.getOctokit(token);
 
-  const pubEvents = await  octokit.rest.issues.listEvents({
-    owner,
-    repo
-  });
-  console.log(`Repo issue events: ${JSON.stringify(pubEvents)}`);
+  // const pubEvents = await  octokit.rest.issues.listEvents({
+  //   owner,
+  //   repo
+  // });
+  // console.log(`Repo issue events: ${JSON.stringify(pubEvents)}`);
 } catch (error) {
   core.setFailed(error.message);
 }
