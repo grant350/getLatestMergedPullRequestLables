ed7eef9788d9729f2acacbc2c3700642f985dd8c
Grant Mitchell <35341268+grant350@users.noreply.github.com>
2023-01-05 20:26:58 -0700

    Update action.js

--- action.js
+++ action.js
@@ -6,35 +6,17 @@ const main = async () => {
 try {
   const payload = github.context.payload;
   const event = core.getInput('event');
-  console.log('username',event.pusher.name);
   const owner = event.repository.owner.name
   const repo = event.repository.name
   const token = core.getInput('token', { required: true });
   const octokit = new github.getOctokit(token);
-
   const pubEvents = await  octokit.rest.issues.listEvents({
     owner,
     repo
   });
-  console.log(`Repo issue events: ${JSON.stringify(pubEvents)}`);
 } catch (error) {
   core.setFailed(error.message);
 }
 
 }
-main(); // run fn
-// https://api.github.com/repos/grant350/kuberform/issues/events [events]
-// object.issue.labels
-
-
-// https://api.github.com/repos/grant350/kuberform/pulls/2
-// object.labels //[]
-// -->   {
-//   "id": 4921402286,
-//   "node_id": "LA_kwDOIc0cy88AAAABJVajrg",
-//   "url": "https://api.github.com/repos/grant350/kuberform/labels/patch",
-//   "name": "patch",
-//   "color": "E55659",
-//   "default": false,
-//   "description": "versionControl patch"
-// }
+main(); 
